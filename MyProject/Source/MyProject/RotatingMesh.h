db9a3f0b7bdfb2ed7af8303e79918a807f8e74a6
Mandy_Fu <man.chunfu@futuregames.nu>
2020-01-07 13:31:10 +0100

    RotatingMesh and Interface BP

---
+++ MyProject/Source/MyProject/RotatingMesh.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DamageableInterface.h"
+#include "Engine/StaticMeshActor.h"
+#include "RotatingMesh.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API ARotatingMesh : public AStaticMeshActor, public IDamageableInterface
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Object Movement")
+	FRotator RotationPerSecond = FRotator(0.0f, 90.0f, 0.0f);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health")
+	float CurrentHealth = 10.0f;
+	ARotatingMesh();
+
+	virtual void Tick(float DeltaSeconds) override;
+	virtual float ApplyDamage_Implementation(float InDamage) override;
+};
